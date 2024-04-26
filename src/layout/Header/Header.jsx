68ff5ed3027c881de5dda9b0dcf46265483b844a
kuharenko-olya <olyavolk49@gmail.com>
2024-04-26 14:31:25 +0300

    add structure, fonts, update vite-config for svg

---
+++ src/layout/Header/Header.jsx
@@ -0,0 +1,9 @@
+import styles from './sass/Header.module.scss';
+import { ReactComponent as Logo } from '../../assets/svg/logo.svg';
+
+
+export function Header() {
+    return (
+          <Logo/>
+    )
+}
